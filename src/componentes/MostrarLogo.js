76487856fc8b0b3f21c9459762ef4387eec6a9cf
Martin Conenna <117860765+martinredondo89@users.noreply.github.com>
2023-11-07 02:18:35 -0300

    primer commit

---
+++ src/componentes/MostrarLogo.js
@@ -0,0 +1,14 @@
+import freeCodeCampLogo from '../imagenes/freecodecamp-logo.png';
+
+function MostrarLogo() {
+  return (
+    <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+        />
+      </div>
+  );
+}
+
+export default MostrarLogo;
